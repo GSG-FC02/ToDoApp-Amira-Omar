fc00e82f6472c2b2e2c0f75ff046c7276037d3b4
AmiraAlnajjar <princess.mmedia@gmail.com>
2021-03-26 16:56:19 +0200

    add initial values Relates #9

---
+++ script.js
@@ -0,0 +1,28 @@
+// Select the Elements
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+const title = document.getElementById("todo-title");
+
+// Classes names
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle";
+const LINE_THROUGH = "lineThrough";
+
+// Variables
+let LIST, id;
+
+// get item from localstorage
+let data = localStorage.getItem("TODO");
+
+// check if data is not empty
+if(data){
+    LIST = JSON.parse(data);
+    id = LIST.length; // set the id to the last one in the list
+    loadList(LIST); // load the list to the user interface
+}else{
+    // if data isn't empty
+    LIST = [];
+    id = 0;
+}
